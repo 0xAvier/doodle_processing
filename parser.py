c36495115b52e2cb65823336b473d51382815496
Xavier P <xavier@26lights.com>
2022-01-02 16:17:32 +0100

    Translate dates

--- parser.py
+++ parser.py
@@ -172,15 +172,51 @@ class Agenda:
         if value == "OK":
           self.events[j + offset_dates].add_player(player)
 
+  def translate_month(self, date):
+    month_dict = {
+      "January": "Jan.", 
+      "February": "Fév.", 
+      "March": "Mars", 
+      "April": "Avr.", 
+      "May": "Mai", 
+      "June": "Juin", 
+      "July": "Juil.", 
+      "August": "Août", 
+      "September": "Sept.", 
+      "October": "Oct.", 
+      "November": "Nov.", 
+      "December": "Déc."
+    }
+    res = date 
+    for m in month_dict.keys():
+      res = res.replace(m, month_dict[m])
+    return res 
+
+
+  def translate_day(self, date):
+    day_dict = {
+      "Mon": "Lun.", 
+      "Tue": "Mar.", 
+      "Wed": "Mer.", 
+      "Thu": "Jeu.", 
+      "Fri": "Ven.", 
+      "Sat": "Sam.", 
+      "Sun": "Dim."
+    }
+    res = date 
+    for d in day_dict.keys():
+      res = res.replace(d, day_dict[d])
+    return res 
+
 
   def _parse_dates(self, row, sh):
     for j in range(1, sh.ncols):
       value = sh.cell(row-1, j).value
-    current_month = sh.cell(row-1, 1).value
+    current_month = self.translate_month(sh.cell(row-1, 1).value)
     for j in range(1, sh.ncols):
       if (v := sh.cell(row-1, j).value) != "":
-        current_month = v
-      value = sh.cell(row, j).value
+        current_month = self.translate_month(v)
+      value = self.translate_day(sh.cell(row, j).value)
       self.add_event(Event(value + " " + current_month))
 
 
@@ -195,5 +231,4 @@ def find_matches(agenda, collection, group):
 group, collection = parse_config('config')
 agenda = Agenda('/home/xavier/Bureau/Doodle.xls')
 find_matches(agenda, collection, group)
-# TODO: translate date
 print(agenda)
