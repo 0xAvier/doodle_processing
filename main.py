9212af462ae387f00cca9d1cedf894a894200130
Xavier P <xavier@26lights.com>
2022-04-01 16:48:27 +0200

    feat: Rename args

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def parse_args():
   parser = argparse.ArgumentParser(description='Find matching')
   parser.add_argument('-s', '--sheet', required=True)
   parser.add_argument('-c', '--config', default='config')
-  parser.add_argument('-i', '--create_ics',  action="store_true", description='Parse sheet to create the given ics')
+  parser.add_argument('-i', '--create_ics',  action="store_true", help='Parse sheet to create the given ics')
   return parser.parse_args()
 
 
@@ -20,7 +20,7 @@ configuration = Configuration(args.config)
 
 # TODO rename agenda in Availabilities
 # TODO move configuration to agenda
-if args.parse:
+if args.create_ics:
   calendar = GameCalendar(args.sheet, configuration)
   calendar.write_calendars(configuration)
 else:
