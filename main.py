e0bad964829181e8add227e2141cf694ed7676e7
Xavier P <xavier@26lights.com>
2022-06-23 14:59:09 +0200

    feat: Add config generation from poll
    Refs: #6

--- main.py
+++ main.py
@@ -2,15 +2,17 @@
 import argparse
 
 from src.Configuration import Configuration
+from src.ConfigFromPoll import generateConfigFromPoll  
 from src.Agenda import Agenda
 from src.GameCalendar import GameCalendar 
 
 
 def parse_args():
   parser = argparse.ArgumentParser(description='Find matching')
-  parser.add_argument('-s', '--sheet', required=True)
+  parser.add_argument('-s', '--sheet')
   parser.add_argument('-c', '--config', default='config')
   parser.add_argument('-i', '--create_ics',  action="store_true", help='Parse sheet to create the given ics')
+  parser.add_argument('--generate_config_from_poll', help='Generate the config file from poll. No other option are relevant.')
   return parser.parse_args()
 
 
@@ -21,7 +23,9 @@ configuration = Configuration(args.config)
 
 # TODO rename agenda in Availabilities
 # TODO move configuration to agenda
-if args.create_ics:
+if args.generate_config_from_poll:
+  generateConfigFromPoll(args.generate_config_from_poll)
+elif args.create_ics:
   calendar = GameCalendar(args.sheet, configuration)
   calendar.write_calendars(configuration)
 else:
---
+++ src/ConfigFromPoll.py
@@ -0,0 +1,44 @@
+import csv
+import subprocess
+
+
+def read_games(game_line):
+  return list(map(lambda s: s.replace(" ", ""), game_line))
+
+
+def display_results(games, players, o_file):
+  with open(o_file, 'w') as my_file:
+    print_games(games, my_file)
+    print_players(players, my_file)
+
+
+def print_games(games, my_file):
+  my_file.write("[games]\n")
+  for g in games:
+    if g != "":
+      my_file.write(f"{g}=\n")
+
+  my_file.write("\n")
+
+
+def print_players(players, my_file):
+  my_file.write("[players]\n")
+  for key, value in players.items():
+    my_file.write(f"{key}={','.join(value)}\n")
+
+
+def generateConfigFromPoll(filename, o_file="output"):
+  subprocess.call(["sed", "-i", "-e",  's/"//g', filename])
+  players = {}
+  with open(filename, newline='') as csvfile:
+      sh = list(csv.reader(csvfile, delimiter=',', quotechar='"'))
+      n_row = len(list(sh))
+      n_col = len(list(sh[0]))
+      games = read_games(sh[0][1:])
+      for i in range(1, n_row):
+        player_name = sh[i][0]
+        players[player_name] = []
+        for j in range(1, n_col):
+          if sh[i][j] == "Oui" or sh[i][j] == "Si nécessaire":
+            players[player_name].append(games[j-1])
+      display_results(games, players, o_file)
